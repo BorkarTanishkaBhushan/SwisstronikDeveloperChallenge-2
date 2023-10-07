3d03fdc4af55dea41654cbc3b851816e3f887dba
tanishkaBBR <tanishka.b@somaiya.edu>
2023-10-07 11:00:11 +0530

    Added mumbai testnet deploy and get storage script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    }
+  },
 };
